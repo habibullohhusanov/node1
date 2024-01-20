0c1a72ec23144d3a589f4863287f7f2fb015ecc4
Habibulloh <husanovhabibullohhusanov@gmail.com>
2024-01-21 01:03:27 +0500

    .env added

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+require("dotenv").config()
+
+const PORT = process.env.PORT;
+const URL = process.env.URL;
+const DB_URL = process.env.DB_URL;
 const mongoose = require("mongoose")
-const PORT = process.env.PORT || 5000;
 const userRouter = require("./route/user");
 const urlParse = require("./middlware/urlParse");
 const jsonParse = require("./middlware/jsonParse");
@@ -8,16 +12,16 @@ const Applications = require("./public/Applications");
 const app = new Applications();
 
 app.use(jsonParse);
-app.use(urlParse("http://localhost:5000"));
+app.use(urlParse(`${URL}:${PORT}`));
 app.addRoute(userRouter);
 
 
 const start = async () => {
     try {
-        await mongoose.connect("mongodb+srv://habibullohhusanov:123ABS@node.ozdqkl1.mongodb.net/?retryWrites=true&w=majority");
+        await mongoose.connect(DB_URL);
         app.listen(PORT);
     } catch(e) {
-        //
+        console.log(e);
     }
 }
 start();
